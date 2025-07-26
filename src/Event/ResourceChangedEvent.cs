1b0e4414c6ba7172c13535b9ac4efdd709ad85ab
starlight <ss14-Starlight@outlook.com>
2025-07-26 06:15:26 +0200

    Event bus

---
+++ src/Event/ResourceChangedEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Starlight.NullLink.Event;
+public sealed record ResourceChangedEvent
+{
+    public required Guid Player { get; init; }
+    public required string Resource { get; init; }
+    public required double NewAmount { get; init; }
+}
---
+++ src/IEventBusObserver.cs
@@ -0,0 +1,13 @@
+﻿using Orleans.Concurrency;
+using Starlight.NullLink.Event;
+
+namespace Starlight.NullLink;
+
+[Alias("Starlight.NullLink.IEventBusObserver")]
+public interface IEventBusObserver : IGrainObserver
+{
+
+    [OneWay]
+    [Alias("OnEventReceived<T>")]
+    ValueTask OnEventReceived<T>(T @event) where T : BaseEvent;
+}
