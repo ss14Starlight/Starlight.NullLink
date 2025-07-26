e90edc3832cf0e6c4cb35d776810750369be708b
starlight <ss14-Starlight@outlook.com>
2025-07-26 02:48:36 +0200

    PlayerData

---
+++ src/Color.cs
@@ -0,0 +1,15 @@
+﻿namespace Starlight.NullLink;
+
+[GenerateSerializer]
+[Alias("Color")]
+public sealed record Color
+{
+    public static Color White = new() { R = 255, G = 255, B = 255 };
+
+    [Id(0)]
+    public byte R { get; set; }
+    [Id(1)]
+    public byte G { get; set; }
+    [Id(2)]
+    public byte B { get; set; }
+}
--- src/IServerGrain.cs
+++ src/IServerGrain.cs
@@ -12,6 +12,34 @@ public interface IServerGrain : IGrainWithStringKey
     [Public, Alias("UpdateServerInfo")]
     public ValueTask UpdateServerInfo(ServerInfo info);
 
-    [Public, Alias("GetRoles")]
-    public ValueTask<ulong[]> GetPlayerRoles(Guid player);
+
+    [Public, Alias("GetPlayerData")]
+    public ValueTask<PlayerData> GetPlayerData(Guid player);
+
+    [Public, Alias("SetGhostTheme")]
+    public ValueTask SetGhostTheme(Guid player, GhostTheme theme);
+
+    [Public, Alias("UpdateResource")]
+    public ValueTask UpdateResource(Guid player, string key, double value);
+}
+
+[GenerateSerializer]
+[Alias("Starlight.NullLink.PlayerData")]
+public sealed class PlayerData
+{
+    [Id(0)]
+    public GhostTheme GhostTheme = new();
+    [Id(1)]
+    public Dictionary<string, double> Resources = [];
+}
+
+[GenerateSerializer]
+[Alias("Starlight.NullLink.GhostTheme")]
+public sealed record GhostTheme
+{
+    [Id(0)]
+    public string Id { get; set; } = "None";
+
+    [Id(1)]
+    public Color GhostThemeColor = Color.White;
 }
