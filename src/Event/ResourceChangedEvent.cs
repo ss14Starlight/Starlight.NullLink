602afc1973cfbc3c8a8b8902e2b85145204483be
starlight <ss14-Starlight@outlook.com>
2025-07-28 07:48:37 +0200

    events

--- src/Event/ResourceChangedEvent.cs
+++ src/Event/ResourceChangedEvent.cs
@@ -5,9 +5,14 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Starlight.NullLink.Event;
+[GenerateSerializer]
+[Alias("Starlight.NullLink.Event.ResourceChangedEvent")]
 public sealed record ResourceChangedEvent : BaseEvent
 {
+    [Id(0)]
     public required Guid Player { get; init; }
+    [Id(1)]
     public required string Resource { get; init; }
+    [Id(2)]
     public required double NewAmount { get; init; }
 }
