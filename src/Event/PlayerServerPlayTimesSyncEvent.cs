958b16cd1aeac95293638854a78601df20220f5d
starlight <ss14-Starlight@outlook.com>
2025-12-10 07:26:51 +0100

    Add PlayerServerPlayTimesSyncEvent for syncing player play times

---
+++ src/Event/PlayerServerPlayTimesSyncEvent.cs
@@ -0,0 +1,11 @@
+﻿namespace Starlight.NullLink.Event;
+
+[GenerateSerializer]
+[Alias("Starlight.NullLink.Event.PlayerServerPlayTimesSyncEvent")]
+public sealed record PlayerServerPlayTimesSyncEvent : BaseEvent, IPlayerEvent
+{
+    [Id(0)]
+    public required Guid Player { get; init; }
+    [Id(1)]
+    public required Dictionary<string, PlayTime[]> ServerPlayTimes { get; init; }
+}
