10a8d2b9a7ad489a3814cbc591ed876379eed4a7
Rinary <rinary.super@gmail.com>
2026-01-04 21:34:48 +0200

    init

---
+++ src/Event/PlayerResourcesSyncEvent.cs
@@ -0,0 +1,11 @@
+namespace Starlight.NullLink.Event;
+
+[GenerateSerializer]
+[Alias("Starlight.NullLink.Event.PlayerServerPlayTimesSyncEvent")]
+public sealed record PlayerResourcesSyncEvent : BaseEvent, IPlayerEvent
+{
+    [Id(0)]
+    public required Guid Player { get; init; }
+    [Id(1)]
+    public required Dictionary<string, double> Resources { get; init; }
+}
