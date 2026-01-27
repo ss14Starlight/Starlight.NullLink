fa697816bf7a96dfa3955decb27d71e24c4cf64a
Rinary <rinary.super@gmail.com>
2026-01-27 21:41:28 +0200

    upd

--- src/AdminNote.cs
+++ src/AdminNote.cs
@@ -5,41 +5,41 @@ namespace Starlight.NullLink;
 public record struct AdminNote
 {
     [Id(0)]
-    int Id { get; set; }
+    public int Id { get; set; }
     [Id(1)]
-    Guid Player { get; set; }
+    public Guid Player { get; set; }
     [Id(2)]
-    string ProjectName { get; set; }
+    public string ProjectName { get; set; }
     [Id(3)]
-    string ServerName { get; set; }
+    public string ServerName { get; set; }
     [Id(4)]
-    int? Round { get; set; }
+    public int? Round { get; set; }
     [Id(5)]
-    TimeSpan PlaytimeAtNote { get; set; }
+    public TimeSpan PlaytimeAtNote { get; set; }
     [Id(6)]
-    string NoteType { get; set; }
+    public string NoteType { get; set; }
     [Id(7)]
-    string Message { get; set; }
+    public string Message { get; set; }
     [Id(8)]
-    string? NoteSeverity { get; set; }
+    public string? NoteSeverity { get; set; }
     [Id(9)]
-    bool Secret { get; set; }
+    public bool Secret { get; set; }
     [Id(10)]
-    string CreatedByName { get; set; }
+    public string CreatedByName { get; set; }
     [Id(11)]
-    string EditedByName { get; set; }
+    public string EditedByName { get; set; }
     [Id(12)]
-    DateTime CreatedAt { get; set; }
+    public DateTime CreatedAt { get; set; }
     [Id(13)]
-    DateTime? LastEditedAt { get; set; }
+    public DateTime? LastEditedAt { get; set; }
     [Id(14)]
-    DateTime? ExpiryTime { get; set; }
+    public DateTime? ExpiryTime { get; set; }
     [Id(15)]
-    string[]? BannedRoles { get; set; }
+    public string[]? BannedRoles { get; set; }
     [Id(16)]
-    DateTime? UnbannedTime { get; set; }
+    public DateTime? UnbannedTime { get; set; }
     [Id(17)]
-    string? UnbannedByName { get; set; }
+    public string? UnbannedByName { get; set; }
     [Id(18)]
-    bool? Seen { get; set; }
+    public bool? Seen { get; set; }
 }
