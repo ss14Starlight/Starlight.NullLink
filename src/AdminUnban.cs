cc0a432e9c2d641e3dade4f2e16f745037092b28
Rinary <rinary.super@gmail.com>
2026-03-28 08:31:00 +0200

    project/server names into unban

--- src/AdminUnban.cs
+++ src/AdminUnban.cs
@@ -2,7 +2,7 @@ namespace Starlight.NullLink;
 
 [GenerateSerializer]
 [Alias("AdminUnban")]
-public record struct AdminUnban(int banId, Guid? unbanningAdmin, DateTimeOffset unbanTime)
+public record struct AdminUnban(int banId, Guid? unbanningAdmin, DateTimeOffset unbanTime, string? projectName, string? serverName)
 {
     [Id(0)]
     public int BanId { get; } = banId;
@@ -10,4 +10,8 @@ public record struct AdminUnban(int banId, Guid? unbanningAdmin, DateTimeOffset 
     public Guid? UnbanningAdmin { get; } = unbanningAdmin;
     [Id(2)]
     public DateTimeOffset UnbanTime { get; } = unbanTime;
+    [Id(3)]
+    public string? ProjectName = projectName;
+    [Id(4)]
+    public string? ServerName = serverName;
 }
