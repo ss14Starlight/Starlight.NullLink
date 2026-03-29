c39aec3c38a7118f830cacc973cf5f767ff80532
Rinary <rinary.super@gmail.com>
2026-03-29 12:08:48 +0300

    rework to bson class

--- src/AdminBan.cs
+++ src/AdminBan.cs
@@ -1,3 +1,4 @@
+using MongoDB.Bson.Serialization.Attributes;
 using System.Collections.Immutable;
 using System.Net;
 
@@ -5,9 +6,11 @@ namespace Starlight.NullLink;
 
 [GenerateSerializer]
 [Alias("AdminBan")]
-public record struct AdminBan
+[BsonIgnoreExtraElements]
+public class AdminBan
 {
-    public AdminBan(int? id, Guid? userId, (IPAddress, int)? address, (ImmutableArray<byte> hwid, int type)? hwId, DateTimeOffset banTime, DateTimeOffset? expirationTime, int? roundId, TimeSpan playtimeAtNote, string reason, string severity, Guid? banningAdmin, List<AdminUnban> unban, string? role, int? exemptFlags, string? projectName, string? serverName)
+    [BsonConstructor]
+    public AdminBan(int? id, Guid? userId, AddressInfo? address, HwIdInfo? hwId, DateTimeOffset banTime, DateTimeOffset? expirationTime, int? roundId, TimeSpan playtimeAtNote, string reason, string severity, Guid? banningAdmin, List<AdminUnban> unban, string? role, int? exemptFlags)
     {
         if (userId == null && address == null && hwId == null)
             throw new ArgumentException("Why are you trying to create role ban with zero information about user?");
@@ -33,36 +36,62 @@ public record struct AdminBan
         ServerName = serverName;
     }
     [Id(0)]
+    [BsonId]
     public int? Id { get; }
     [Id(1)]
+    [BsonElement("UserId")]
     public Guid? UserId { get; }
     [Id(2)]
-    public (IPAddress address, int cidrMask)? Address { get; }
+    [BsonElement("Address")]
+    public AddressInfo? Address { get; }
     [Id(3)]
-    public (ImmutableArray<byte> hwid, int type)? HWId { get; }
+    [BsonElement("HWId")]
+    public HwIdInfo? HWId { get; }
 
     [Id(4)]
+    [BsonElement("BanTime")]
     public DateTimeOffset BanTime { get; }
     [Id(5)]
+    [BsonElement("ExpirationTime")]
     public DateTimeOffset? ExpirationTime { get; }
     [Id(6)]
+    [BsonElement("RoundId")]
     public int? RoundId { get; }
     [Id(7)]
+    [BsonElement("PlayTimeAtNote")]
     public TimeSpan PlayTimeAtNote { get; }
     [Id(8)]
+    [BsonElement("Reason")]
     public string Reason { get; }
     [Id(9)]
+    [BsonElement("Severity")]
     public string Severity { get; }
     [Id(10)]
+    [BsonElement("BanningAdmin")]
     public Guid? BanningAdmin { get; }
     [Id(11)]
+    [BsonElement("Unban")]
     public List<AdminUnban> Unban { get; }
     [Id(12)]
+    [BsonElement("Role")]
     public string? Role { get; }
     [Id(13)]
+    [BsonElement("ExemptFlags")]
     public int? ExemptFlags { get; }
     [Id(14)]
+    [BsonElement("ProjectName")]
     public string? ProjectName { get; set; }
     [Id(15)]
+    [BsonElement("ServerName")]
     public string? ServerName { get; set; }
+}
+public class AddressInfo
+{
+    public string Address { get; set; } = default!;
+    public int CidrMask { get; set; }
+}
+public class HwIdInfo
+{
+    public byte[] Hwid { get; set; } = default!;
+    public int Type { get; set; }
 }
