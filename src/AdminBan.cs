a9b8cec1a85db72b4cc4caaeec711479a7cd3477
Rinary <rinary.super@gmail.com>
2026-04-19 18:52:42 +0300

    fix admin ban unable to init values

--- src/AdminBan.cs
+++ src/AdminBan.cs
@@ -5,38 +5,38 @@ namespace Starlight.NullLink;
 public class AdminBan
 {
     [Id(0)]
-    public int? Id { get; }
+    public int? Id { get; init; }
     [Id(1)]
-    public Guid? UserId { get; }
+    public Guid? UserId { get; init; }
     [Id(2)]
-    public AddressInfo? Address { get; }
+    public AddressInfo? Address { get; init; }
     [Id(3)]
-    public HwIdInfo? HWId { get; }
+    public HwIdInfo? HWId { get; init; }
 
     [Id(4)]
-    public DateTimeOffset BanTime { get; }
+    public DateTimeOffset BanTime { get; init; }
     [Id(5)]
-    public DateTimeOffset? ExpirationTime { get; }
+    public DateTimeOffset? ExpirationTime { get; init; }
     [Id(6)]
-    public int? RoundId { get; }
+    public int? RoundId { get; init; }
     [Id(7)]
-    public TimeSpan PlayTimeAtNote { get; }
+    public TimeSpan PlayTimeAtNote { get; init; }
     [Id(8)]
-    public string? Reason { get; }
+    public string? Reason { get; init; }
     [Id(9)]
-    public string? Severity { get; }
+    public string? Severity { get; init; }
     [Id(10)]
-    public Guid? BanningAdmin { get; }
+    public Guid? BanningAdmin { get; init; }
     [Id(11)]
-    public List<AdminUnban> Unban { get; } = [];
+    public List<AdminUnban> Unban { get; init; } = [];
     [Id(12)]
-    public string? Role { get; }
+    public string? Role { get; init; }
     [Id(13)]
-    public int? ExemptFlags { get; }
+    public int? ExemptFlags { get; init; }
     [Id(14)]
-    public string? ProjectName { get; set; }
+    public string? ProjectName { get; init; }
     [Id(15)]
-    public string? ServerName { get; set; }
+    public string? ServerName { get; init; }
 }
 
 [GenerateSerializer]
